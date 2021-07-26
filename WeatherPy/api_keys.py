111d4688fc5f61d468eb3050c26206f49ac9e19f
AnilZown <anilkumar_ga@yahoo.com>
2021-07-26 13:47:44 -0400

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "6e4e38e82983305fa677f27d358048fa"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDoWQfvLNb6GPgRwgbiXaLYS_EtYe8q3Ig"
+g_key = ""
